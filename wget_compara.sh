57035b03d31ca0299e1267becf9924e7c5360b66
Qinhu Wang <qinhu.wang@icloud.com>
2017-02-21 11:15:35 +0800

    update database

--- wget_compara.sh
+++ wget_compara.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget ftp://ftp.ensemblgenomes.org//pub/release-32/fungi/tsv/ensembl-compara/Compara.homologies.32.tsv.gz
-gunzip Compara.homologies.32.tsv.gz
+wget ftp://ftp.ensemblgenomes.org//pub/release-34/fungi/tsv/ensembl-compara/Compara.homologies.34.tsv.gz
+gunzip Compara.homologies.34.tsv.gz
 
